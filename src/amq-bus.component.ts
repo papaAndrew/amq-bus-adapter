e9f0911fa78daf1a7c226521c6cb93d2abed6889
papaAndrew <a.hermit71@gmail.com>
2024-01-08 13:28:50 +0500

    fix bindings

--- src/amq-bus.component.ts
+++ src/amq-bus.component.ts
@@ -7,7 +7,7 @@ import {
   injectable,
   LifeCycleObserver,
 } from "@loopback/core";
-import { AmqBusBindings } from "./lib/keys";
+import {AmqBusBindings} from "./lib/keys";
 import {
   AmqBusLogAdapter,
   AmqBusOptions,
@@ -17,14 +17,15 @@ import {
   ErrorHandler,
   ResponseBuilder,
 } from "./lib/types";
-import { AmqBusClientProvider } from "./providers/amq-bus-client.provider";
-import { AmqBusLogAdapterProvider } from "./providers/amq-bus-log-adapter.provider";
-import { AmqBusServerFactoryProvider } from "./providers/amq-bus-server-factory.provider";
-import { AmqConnectorProvider } from "./providers/amq-connector.provider";
-import { FatalErrorHandlerProvider } from "./providers/fatal-error-handler.provider";
-import { ResponseBuilderProvider } from "./providers/response-builder.provider";
+import {AmqBusClientProvider} from "./providers/amq-bus-client.provider";
+import {AmqBusLogAdapterProvider} from "./providers/amq-bus-log-adapter.provider";
+import {AmqBusServerFactoryProvider} from "./providers/amq-bus-server-factory.provider";
+import {AmqConnectorProvider} from "./providers/amq-connector.provider";
+import {FatalErrorHandlerProvider} from "./providers/fatal-error-handler.provider";
+import {ResponseBuilderProvider} from "./providers/response-builder.provider";
+import {ServerContextFactoryProvider} from './providers/server-context-factory.provider';
 
-@injectable({ tags: { [ContextTags.KEY]: AmqBusBindings.COMPONENT } })
+@injectable({tags: {[ContextTags.KEY]: AmqBusBindings.COMPONENT}})
 export class AmqBusComponent implements Component, LifeCycleObserver {
   bindings: Binding<any>[] = [
     Binding.bind(AmqBusBindings.FATAL_ERROR_HANDLER)
@@ -39,6 +40,9 @@ export class AmqBusComponent implements Component, LifeCycleObserver {
     Binding.bind(AmqBusBindings.PRODUCER_CLIENT)
       .toProvider(AmqBusClientProvider)
       .inScope(BindingScope.REQUEST),
+    Binding.bind(AmqBusBindings.SERVER_CONTEXT_FACTORY)
+      .toProvider(ServerContextFactoryProvider)
+      .inScope(BindingScope.SINGLETON),
     Binding.bind(AmqBusBindings.CONSUMER_SERVER_FACTORY)
       .toProvider(AmqBusServerFactoryProvider)
       .inScope(BindingScope.SINGLETON),
@@ -108,7 +112,7 @@ export class AmqBusComponent implements Component, LifeCycleObserver {
 
   private async disconnect() {
     this._connector.disconnect();
-    this._logAdapter.onDisconnect({ cause: "Component stopped" });
+    this._logAdapter.onDisconnect({cause: "Component stopped"});
   }
 
   async start() {
